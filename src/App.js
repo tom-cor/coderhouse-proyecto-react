c6a074456cf4fd433648a09fa10eefe966b5c34b
tom-cor <tomas.cornaglia@gmail.com>
2024-02-27 22:41:37 -0300

    Added the ItemListContainer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navBar/Navbar';
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ItemListContainer greeting={"Vamos a iluminar tu vida con los tonos más coloridos!"}/>
     </div>
   );
 }
